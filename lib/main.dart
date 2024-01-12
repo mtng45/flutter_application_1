40588d9a55c29016c9eda751366edeb76daf8a1e
matsunaga-kenta1 <matsunaga-kenta1@dmm.com>
2024-01-12 18:00:10 +0900

    split to GeneratorPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:namer_app/pages/generator.dart';
 import 'package:namer_app/widgets/big_card.dart';
 import 'package:namer_app/widgets/side_navigation.dart';
 import 'package:provider/provider.dart';
@@ -102,50 +103,6 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ));
     });
-  }
-}
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavorite();
-                  },
-                  icon: Icon(icon),
-                  label: Text('Like')),
-              SizedBox(width: 10),
-              ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: Text('Next')),
-            ],
-          )
-        ],
-      ),
-    );
   }
 }
 
