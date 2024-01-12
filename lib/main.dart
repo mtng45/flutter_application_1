2c0f1fb78c3f3c51666afa25a3b0ce7ee4c91821
matsunaga-kenta1 <matsunaga-kenta1@dmm.com>
2024-01-12 12:03:08 +0900

    add NavigationRail to MyHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,36 @@ class MyAppState extends ChangeNotifier {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        body: Row(
+      children: [
+        SafeArea(
+            child: NavigationRail(
+          extended: false,
+          destinations: [
+            NavigationRailDestination(
+                icon: Icon(Icons.home), label: Text('Home')),
+            NavigationRailDestination(
+                icon: Icon(Icons.favorite), label: Text('Favorites'))
+          ],
+          selectedIndex: 0,
+          onDestinationSelected: (value) {
+            print('selected: $value');
+          },
+        )),
+        Expanded(
+            child: Container(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          child: GeneratorPage(),
+        ))
+      ],
+    ));
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
@@ -61,39 +91,31 @@ class MyHomePage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      /**
-       * Column
-       *  Flutter における非常に基本的なレイアウト ウィジェット
-       *  任意の数の子を従え、それらを上から下へ一列に配置
-       *  デフォルトでは、その子を上に寄せる
-       */
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                    onPressed: () {
-                      appState.toggleFavorite();
-                    },
-                    icon: Icon(icon),
-                    label: Text('Like')),
-                ElevatedButton(
-                    onPressed: () {
-                      appState.getNext();
-                    },
-                    child: Text('Next')),
-              ],
-            )
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like')),
+              ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next')),
+            ],
+          )
+        ],
       ),
     );
   }
