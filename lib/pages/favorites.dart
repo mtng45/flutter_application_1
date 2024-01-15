22a9e3bb260b465009ea1c50206f009da9f24a60
matsunaga-kenta1 <matsunaga-kenta1@dmm.com>
2024-01-15 12:46:25 +0900

    update FavoritesPage

--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     var appState = context.watch<MyAppState>();
     var favorites = appState.favorites;
 
@@ -12,12 +13,12 @@ class FavoritesPage extends StatelessWidget {
       return Center(child: Text('No favorites yet.'));
     }
 
-    return ListView(children: [
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(
-            padding: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(30),
             child: Text('you have' '${favorites.length} favorites'),
           ),
           TextButton(
@@ -27,9 +28,27 @@ class FavoritesPage extends StatelessWidget {
               child: Text('Clear')),
         ],
       ),
-      for (var favorite in favorites)
-        ListTile(
-            leading: Icon(Icons.favorite), title: Text(favorite.asLowerCase)),
+      Expanded(
+          child: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 400, childAspectRatio: 400 / 80),
+        children: [
+          for (var pair in appState.favorites)
+            ListTile(
+              leading: IconButton(
+                icon: Icon(Icons.delete_outline, semanticLabel: 'Delete'),
+                color: theme.colorScheme.primary,
+                onPressed: () {
+                  appState.removeFavorite(pair);
+                },
+              ),
+              title: Text(
+                pair.asLowerCase,
+                semanticsLabel: pair.asPascalCase,
+              ),
+            ),
+        ],
+      ))
     ]);
   }
 }
